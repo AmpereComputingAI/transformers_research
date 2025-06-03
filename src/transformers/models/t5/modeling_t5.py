ea5baa66e5e520afe7a8ae578de38feedbfd1e9c
Jan Grzybek <jan@amperecomputing.com>
2025-06-04 00:56:42 +0200

    wip

--- src/transformers/models/t5/modeling_t5.py
+++ src/transformers/models/t5/modeling_t5.py
@@ -1317,8 +1317,6 @@ class T5Stack(T5PreTrainedModel):
             # hidden-states, key-value-states, (self-attention position bias), (self-attention weights), (cross-attention position bias), (cross-attention weights)
             if use_cache is False:
                 layer_outputs = layer_outputs[:1] + (None,) + layer_outputs[1:]
-                print(layer_outputs)
-                fd
 
             hidden_states, next_decoder_cache = layer_outputs[:2]
 
@@ -1331,8 +1329,6 @@ class T5Stack(T5PreTrainedModel):
 
             if output_attentions:
                 all_attentions = all_attentions + (layer_outputs[3],)
-                print(all_attentions)
-                fd
                 if self.is_decoder:
                     all_cross_attentions = all_cross_attentions + (layer_outputs[5],)
 
@@ -1342,10 +1338,7 @@ class T5Stack(T5PreTrainedModel):
                     if i == v[-1] and "cuda:" + str(k) != self.last_device:
                         hidden_states = hidden_states.to("cuda:" + str(k + 1))
 
-        tracer.summary()
-        ff
-
-        hidden_states = self.final_layer_norm(hidden_states)
+        hidden_states = self.final_layer_norm(tracer, hidden_states)
         hidden_states = self.dropout(hidden_states)
 
         # Add last layer
